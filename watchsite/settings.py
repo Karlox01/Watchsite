9c7db45923dd90bbb81d3fd99dd5909c827b8f56
Karlox01 <orlandinikarlo@gmail.com>
2023-09-27 20:18:50 +0000

    add allowed host to heroku app

--- watchsite/settings.py
+++ watchsite/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'W&Xbk^`BkvDo;1S')
 DEBUG = True
 
 
-CSRF_TRUSTED_ORIGINS=['https://8000-karlox01-watchsite-8le5s951j2c.ws-eu105.gitpod.io','https://8000-karlox01-watchsite-8le5s951j2c.ws-eu104.gitpod.io','https://watches-by-karl-c82b1bd8619b.herokuapp.com/']
-ALLOWED_HOSTS = ['localhost', '8000-karlox01-watchsite-8le5s951j2c.ws-eu105.gitpod.io', '8000-karlox01-watchsite-8le5s951j2c.ws-eu104.gitpod.io','https://watches-by-karl-c82b1bd8619b.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS=['https://watches-by-karl-c82b1bd8619b.herokuapp.com/']
+ALLOWED_HOSTS = ['https://watches-by-karl-c82b1bd8619b.herokuapp.com/', 'localhost', '8000-karlox01-watchsite-8le5s951j2c.ws-eu104.gitpod.io']
 
 
 LOGIN_URL = '/login/'
