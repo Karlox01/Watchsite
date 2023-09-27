cc6a58623f890af073eaffb3e33ee9d70efa0575
Karlox01 <orlandinikarlo@gmail.com>
2023-09-27 21:18:40 +0000

    rolling back still cant pinpoint a fix

--- watchsite/urls.py
+++ watchsite/urls.py
@@ -14,19 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 from django.contrib import admin
-from django.urls import include
+from django.urls import path, include
+
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path('static/<path>', serve, {'document_root': settings.STATIC_ROOT}),
     path('', include('watchapp.urls')),
     path('watches/', include('watches.urls')),
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
