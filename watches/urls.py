579a8b2ffb4404741f7ee26fec0b4daf3e688ff9
Karlox01 <orlandinikarlo@gmail.com>
2023-10-01 19:19:29 +0000

    test for dropdown menu

--- watches/urls.py
+++ watches/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'watches'
 
 urlpatterns = [
-    path('', views.filtered_items, name='items'),  # Change to 'items'
+    path('', views.filtered_items, name='items'), 
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/submit_offer/', views.submit_offer, name='submit_offer'),
-    path('watches/category/<str:category_name>/', views.filtered_items, name='watches_by_category'),  # Change to 'filtered_items'
-    path('watches/filter/price/', views.filtered_items, name='filtered_watches_by_price'),  # Change to 'filtered_items'
+    path('watches/category/<str:category_name>/', views.filtered_items, name='watches_by_category'),  
+    path('watches/filter/price/', views.filtered_items, name='filtered_watches_by_price'), 
     path('filtered_watches_by_year/', views.filtered_items, name='filtered_watches_by_year'),
     path('search/', views.search_items, name='search_items'),
     path('watches/category/<str:category_name>/', views.filtered_items, name='watches_by_category'),
