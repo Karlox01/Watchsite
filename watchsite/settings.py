c55cbf55acc7affe1533516de5f98126741e8ac1
Karlox01 <orlandinikarlo@gmail.com>
2023-10-02 18:59:42 +0000

    debug to false ready for final version

--- watchsite/settings.py
+++ watchsite/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 CSRF_TRUSTED_ORIGINS=['https://watches-by-karl-c82b1bd8619b.herokuapp.com']
